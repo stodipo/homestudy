6f09e96f9aa011d51ebb90ea4396e9bc42a3d3ff
stodipo <odipo87@gmail.com>
2017-11-04 20:48:51 +0300

    Name space todo parrot application

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace todoparrot\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    todoparrot\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    todoparrot\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    todoparrot\Exceptions\Handler::class
 );
 
 /*
